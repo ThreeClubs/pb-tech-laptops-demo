0e6d811f4ce46d2a324d240ba168867251dbad1f
Andrew Grenon <andrew.grenon@gmail.com>
2023-01-09 17:41:37 +1300

    nav top bar started need fix hyperlink disp

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,50 @@
 function Navbar() {
   return (
     <>
-      <div className="h-9 w-full bg-gray-900"></div>
+      <nav className="text-center text-white h-9 w-full bg-gray-900 py-0 px-20">
+        <ul className="pr-64 flex flex-grow-0 justify-start items-center text-xs">
+          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
+            <a className="block flex flex-nowrap" href="#">
+              PB Tech
+            </a>
+          </li>
+          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
+            <a className="block flex flex-nowrap" href="#">
+              Hardwired
+            </a>
+          </li>
+          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-center items-start">
+            <a className="block flex flex-nowrap" href="#">
+              PB Business
+            </a>
+          </li>
+          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
+            <a className="block flex flex-nowrap" href="#">
+              PB Education
+            </a>
+          </li>
+          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
+            <a className="block flex flex-nowrap" href="#">
+              PB Wholesale
+            </a>
+          </li>
+          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
+            <a className="block flex flex-nowrap" href="#">
+              PB Government
+            </a>
+          </li>
+          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
+            <a className="block flex flex-nowrap" href="#">
+              PB Health
+            </a>
+          </li>
+          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
+            <a className="block flex flex-nowrap" href="#">
+              PB IoT
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
