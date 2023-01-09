9c7eb3cf60ccc00801a0d8ac8398a2a3a028c9b8
Andrew Grenon <andrew.grenon@gmail.com>
2023-01-09 22:44:31 +1300

    Finished top Nav bar with respons css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,47 +1,42 @@
 function Navbar() {
   return (
     <>
-      <nav className="text-center text-white h-9 w-full bg-gray-900 py-0 px-20">
-        <ul className="pr-64 flex flex-grow-0 justify-start items-center text-xs">
-          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
-            <a className="block flex flex-nowrap" href="#">
-              PB Tech
-            </a>
+    {/* Top Nav Bar */}
+      <nav className="text-center text-white text-xs h-9 w-full bg-gray-900 py-0 px-20">
+        <div className="h-full w-8 cursor-pointer flex items-center justify-start md:hidden">
+          <svg
+            className="h-full fill-gray-200"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </div>
+        <ul className="justify-start md:flex hidden lg:pr-64 pr-0">
+          <li className="mx-auto leading-9">
+            <a href="#">PB Tech</a>
           </li>
-          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
-            <a className="block flex flex-nowrap" href="#">
-              Hardwired
-            </a>
+          <li className="mx-auto leading-9">
+            <a href="#">Hardwired</a>
           </li>
-          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-center items-start">
-            <a className="block flex flex-nowrap" href="#">
-              PB Business
-            </a>
+          <li className="mx-auto leading-9">
+            <a href="#">PB Business</a>
           </li>
-          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
-            <a className="block flex flex-nowrap" href="#">
-              PB Education
-            </a>
+          <li className="mx-auto leading-9">
+            <a href="#">PB Education</a>
           </li>
-          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
-            <a className="block flex flex-nowrap" href="#">
-              PB Wholesale
-            </a>
+          <li className="mx-auto leading-9">
+            <a href="#">PB Wholesale</a>
           </li>
-          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
-            <a className="block flex flex-nowrap" href="#">
-              PB Government
-            </a>
+          <li className="mx-auto leading-9">
+            <a href="#">PB Government</a>
           </li>
-          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
-            <a className="block flex flex-nowrap" href="#">
-              PB Health
-            </a>
+          <li className="mx-auto leading-9">
+            <a href="#">PB Health</a>
           </li>
-          <li className="border border-white h-8 w-22 mr-16 px-4 pb-1 flex justify-start items-start">
-            <a className="block flex flex-nowrap" href="#">
-              PB IoT
-            </a>
+          <li className="mx-auto leading-9">
+            <a href="#">PB IoT</a>
           </li>
         </ul>
       </nav>
